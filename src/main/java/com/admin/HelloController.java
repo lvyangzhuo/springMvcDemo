2bac40532b962aef68a64a597d10dbc69141ff8a
lvyangzhuo@163.com <lvyangzhuo123>
2017-12-07 13:50:04 +0800

    添加示例

--- src/main/java/com/admin/HelloController.java
+++ src/main/java/com/admin/HelloController.java
@@ -2,6 +2,7 @@ package com.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by lyz on 2017-10-5.
@@ -12,6 +13,12 @@ public class HelloController {
     @RequestMapping("/")
     public String hello() {
         return "index";
+    }
+
+    @RequestMapping("/demo1")
+    @ResponseBody
+    public String demo(String a){
+        return a.replaceAll("\\s*","");
     }
 
 }
